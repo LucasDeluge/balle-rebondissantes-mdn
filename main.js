83aa5f6a37aee78caf80f710c1293877099f62be
LucasDeluge <deluge999@gmail.com>
2022-05-06 08:57:45 +0200

    changes on ball number/speed

--- main.js
+++ main.js
@@ -73,15 +73,15 @@ class Ball {
 
 const balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 75) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
     // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
+    random(-8, 8),
+    random(-8, 8),
     randomRGB(),
     size
   );
